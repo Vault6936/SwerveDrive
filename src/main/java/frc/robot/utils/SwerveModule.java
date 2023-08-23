c94d8aaaa0e5efa236e1e25571f0c02768d1f545
2amateurs <samuelegeelhood@gmail.com>
2023-08-23 17:37:53 -0400

    Patch magnitude error in swerve module drive function

--- src/main/java/frc/robot/utils/SwerveModule.java
+++ src/main/java/frc/robot/utils/SwerveModule.java
@@ -34,18 +34,32 @@ public class SwerveModule<T extends MotorController> {
     }
 
     public void calibrateWheels() {
-        isCalibrating = Math.abs(drive(0, 0)) < 0.0087;
+        isCalibrating = Math.abs(drive(0, 0)) < 0.0087; //Half of a degree
     }
 
     public boolean doneCalibrating() {
         return isCalibrating;
     }
 
+    private double minimumMagnitude(double... values) {
+        double min = Double.POSITIVE_INFINITY;
+        for (double value : values) {
+            if (Math.abs(value) < Math.abs(min)) min = value;
+        }
+        return min;
+    }
+
+    /***
+     *
+     * @param speed The speed to set the drive motor to.
+     * @param targetAngle The desired angle, in radians, of the module.
+     * @return The angle of the wheel in radians.
+     */
     public double drive(double speed, double targetAngle) {
         targetAngle = targetAngle % (2 * Math.PI);
         if (targetAngle < 0) targetAngle += 2 * Math.PI;
         double currentAngle = encoder.getAbsolutePosition() * Math.PI / 180 - encoderOffsetAngle;
-        double err = Math.min(Math.min(targetAngle - currentAngle, targetAngle + 2 * Math.PI - currentAngle), targetAngle - 2 * Math.PI - currentAngle); //By always finding the minimum error, this line of code will avoid the type of problems that can occur when the target angle is something like 30 degrees and the wheel angle is 300.  This code would interpret the target angle as 390 degrees and calculate the error out to 90 degrees instead of 270.  Much better :)
+        double err = minimumMagnitude(targetAngle - currentAngle, targetAngle + 2 * Math.PI - currentAngle, targetAngle - 2 * Math.PI - currentAngle); //By always finding the minimum error, this line of code will avoid the type of problems that can occur when the target angle is something like 30 degrees and the wheel angle is 300.  This code would interpret the target angle as 390 degrees and calculate the error out to 90 degrees instead of 270.  Much better :)
         driveMotor.set(speed);
         steeringMotor.set(MathUtil.clamp(controller.calculate(0, err), -1.0, 1.0));
         return currentAngle;
